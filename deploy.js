f0c38f04991a4101c26750b5194f231da2495c27
Jaron Morris <jaron.c.morris@gmail.com>
2022-05-10 16:31:36 -0800

    automated

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '3.84.197.194',
+      host: '172.31.31.223',
       username: 'ubuntu',
       privateKey: 'finalp-key.pem'
     })
